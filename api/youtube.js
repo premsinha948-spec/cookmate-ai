6ab7e59df2c5b8f8a816d603105906f54b5f03ab
premsinha948-spec <premsinha948@gmail.com>
2026-06-08 15:43:30 +0530

    fix youtube embeddable

--- api/youtube.js
+++ api/youtube.js
@@ -7,7 +7,7 @@ module.exports = async function handler(req, res) {
   try {
     const { q } = req.query;
     const key = process.env.REACT_APP_YOUTUBE_KEY || process.env.YOUTUBE_KEY;
-    const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q+" recipe")}&type=video&maxResults=4&key=${key}&regionCode=IN`;
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q+" recipe")}&type=video&maxResults=4&key=${key}&regionCode=IN&videoEmbeddable=true`;
     const r = await fetch(url);
     const d = await r.json();
     res.status(200).json(d);
