8ecfa3d80346664c88fe3a203035036b71f57a68
premsinha948-spec <premsinha948@gmail.com>
2026-06-12 14:40:29 +0530

    fix: manifest.json update kiya PWA ke liye

---
+++ cookmate-android/app/src/main/java/app/vercel/cookmate_ai_xi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.cookmate_ai_xi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
